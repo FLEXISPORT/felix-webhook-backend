d626db1e79d9b9868f3b9e50f72aeb0dc4c11cb5
FLEXISPORT <flexioficina@gmail.com>
2025-07-26 17:39:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Middleware para analizar cuerpo JSON
-app.use(bodyParser.json());
-
-// Ruta de verificación de Webhook
-app.get('/webhook', (req, res) => {
-  const VERIFY_TOKEN = 'FELIX_SECRET_TOKEN';
-
-  const mode = req.query['hub.mode'];
-  const token = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
-
-  if (mode && token === VERIFY_TOKEN) {
-    console.log('WEBHOOK_VERIFIED');
-    res.status(200).send(challenge);
-  } else {
-    res.sendStatus(403);
-  }
-});
-
-// Inicia el servidor
-app.listen(PORT, () => {
-  console.log(`El servidor se está ejecutando en el puerto ${PORT}`);
-});
+export default function handler(req, res) {
+  res.status(200).json({ message: "¡Hola desde F.E.L.I.X. y FLEXISPORT!" });
+}
